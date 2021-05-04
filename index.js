86c62e0daa48f0832e5a78f34d1fa79beb9c132f
santiago <san.tiago.s@hotmail.com>
2021-05-04 15:41:02 +0300

    - api crud & login

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const { Router } = require('express');
 const express = require('express');
+const cors = require('cors');
+const router=require("./app/routers/index");
 const app = express();
 
-const router=require("./app/routers/index");
+app.use(express.json());
+app.use(cors());
 app.use('/',router);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  });
 
   const PORT=3001
   app.listen(PORT, () => {
